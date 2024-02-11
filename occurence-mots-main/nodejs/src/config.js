0370f34252b53ebd8cc4324444e2a1eeab7b95b9
Tony <tony.guan@etu.u-paris.fr>
2024-02-11 22:35:38 +0100

    Update config.js

--- occurence-mots-main/nodejs/src/config.js
+++ occurence-mots-main/nodejs/src/config.js
@@ -1,12 +1,42 @@
-export const redisOptions = {
-    url: "redis://myredis:6379",
-    password: "redispwd"
-}
-export const allNotification = '__keyspace@0__:*'
+import 'dotenv/config'
 
-export const httpOptions = {
-    cors: {
-        origin: '*',
+
+export const getConfigNumber = () => {
+
+    let signe = process.env.SIGNED;
+
+    if (signe === "false" || signe !== "true")
+        signe = false
+    else signe = signe === "true"
+
+    return {
+        integerSize: Number(process.env.INTEGER_SIZE),
+        decimalValue: Number(process.env.DECIMAL_VALUE),
+        signed: signe
     }
 }
-export const canalSocket = "NodeRedis"
+
+export const getTypeMessage = () => {
+    return process.env.MESSAGE
+}
+
+export const getTimeOut = () => {
+    return Number(process.env.PERIODE_MS) || 5000
+
+}
+
+export const getTopic = () => {
+    return process.env.TOPIC || "topic-test"
+}
+
+export const getDebug = () => {
+    return process.env.DEBUG === "true"
+}
+
+export const getLocalBroker = () => {
+    return process.env.HOST_IP !== ""
+}
+
+export const getNumberWord = () => {
+    return process.env.NUMBER_WORD
+}
