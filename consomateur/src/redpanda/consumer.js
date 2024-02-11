e97b70a5b0c0b65c6f8bdbfa37f0ad3beabf9ca9
Tony <tony.guan@etu.u-paris.fr>
2024-02-11 23:20:14 +0100

    Update consumer.js

--- consomateur/src/redpanda/consumer.js
+++ consomateur/src/redpanda/consumer.js
@@ -1,6 +1,8 @@
-import {Kafka, logLevel} from "kafkajs";
-import {getTopic, getLocalBroker} from "../config/config.js";
-import { convertTimestamp } from "./utils.js";
+import {Kafka, logLevel} from "kafkajs"
+import {getTopic, getLocalBroker} from "../config/config.js"
+import { convertTimestamp } from "./utils.js"
+import {createClient} from 'redis'
+import {redisOptions} from "./config/configRedis.js"
 
 const isLocalBroker = getLocalBroker()
 const redpanda = new Kafka({
@@ -36,6 +38,50 @@ export async function connection() {
 
 
 connection()
+
+async function connexion() {
+    try {
+        await consumer.connect()
+        await consumer.subscribe({ topic: getTopic(), fromBeginning: true })
+
+        await consumer.run({
+            eachMessage: async ({ topic, partition, message }) => {
+                const messageJson = JSON.parse(message.value)
+
+                const tabMot = messageJson.message.split(" ")
+
+                tabMot.forEach((mot) => {
+                    client.incr(mot, (err, newValue) => {
+                        if (err) {
+                            console.error('Erreur', err)
+                        } else {
+                            console.log('Val + 1 :', newValue)
+                        }
+                    })
+                })
+
+                console.log({
+                    value: messageJson.message,
+                    timestamp: formatDateAndTime(parseInt(message.timestamp))
+                });
+            },
+        })
+
+    } catch (error) {
+        console.error("Erreur:", error)
+    }
+}
+
+function formatDateAndTime(milliseconds) {
+    const dateObj = new Date(milliseconds)
+    const day = String(dateObj.getDate()).padStart(2, '0')
+    const month = String(dateObj.getMonth() + 1).padStart(2, '0')
+    const year = dateObj.getFullYear()
+    const hours = String(dateObj.getHours()).padStart(2, '0')
+    const minutes = String(dateObj.getMinutes()).padStart(2, '0')
+
+    return `${day}/${month}/${year} à ${hours}:${minutes}`
+}
 
 
 
